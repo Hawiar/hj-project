c098597bee62ba82a36c9743401fe8416c3adf75
themuffinboyy <hollyj71@students.rowan.edu>
2015-10-25 19:40:22 -0400

    User Auth

---
+++ app/admin/user.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register User do
+  permit_params :email, :password
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    actions
+  end
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :email
+      f.input :password
+    end
+    f.actions
+  end
+
+end
