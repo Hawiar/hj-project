f678fddbe6f3c4a9dc1d2a76d0742afa9e3c5a1b
Hawiar <Hawiar6@gmail.com>
2015-10-25 16:31:22 -0400

    users scaf

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020190156) do
+ActiveRecord::Schema.define(version: 20151025201105) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -45,5 +45,13 @@ ActiveRecord::Schema.define(version: 20151020190156) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
