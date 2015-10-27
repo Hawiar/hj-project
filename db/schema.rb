6cffe623253a487b62a0afe3e8b4cb2b495d1e9f
themuffinboyy <hollyj71@students.rowan.edu>
2015-10-26 22:10:23 -0400

    login views and user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025225313) do
+ActiveRecord::Schema.define(version: 20151026235036) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -61,6 +61,10 @@ ActiveRecord::Schema.define(version: 20151025225313) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "high_score"
+    t.integer  "points"
+    t.integer  "wins"
+    t.integer  "losses"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
